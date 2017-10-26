f895ccc17c561ee04374596d34f427c239c80755
IP159997 <IP159997@RDPT-R90K027J.siege.red>
2017-10-26 09:43:58 +0100

    Created class validar.

---
+++ src/main/java/com/redoutevant/gitproject/ValidarLogin.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.redoutevant.gitproject;
+
+/**
+ *
+ * @author Luís Oliveira
+ * @serial 2171026
+ */
+public class ValidarLogin {
+
+    private String username;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
